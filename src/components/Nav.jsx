a84cc334495ffaf6c0c6d6dc131c59c02336186f
Fajri Prawira <fajri.prawira95@gmail.com>
2024-08-30 21:22:15 +0700

    done2

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav() {
       <nav className="navbar sticky top-0 z-10 p-3 bg-gray-100 shadow">
         <div className="navbar-start">
           <Link to="/" className="text-2xl font-bold px-6">
-            <span className="text-accent">Hooman Ma Slave</span>
+            <span className="text-accent">Stradivarius</span>
           </Link>
         </div>
         <div className="navbar-end"></div>
